856ce9e423569e30ba6ed0ed09fd86fc3fa9c093
narandza <60319154+narandza@users.noreply.github.com>
2024-07-26 15:04:32 +0200

    add cta button to landing page, add fonts, fix bg color

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -2,7 +2,7 @@ const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full bg-slate-100">
       {/* todo: navbar */}
-      <main className="pt-40 pb-20 bg-slate-200">{children}</main>
+      <main className="pt-40 pb-20 bg-slate-100">{children}</main>
       {/* todo: footer */}
     </div>
   );
